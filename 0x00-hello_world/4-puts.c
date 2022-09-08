472998ac7b12e6e8bb0e81dd62830858355a8fd2
leroysb <leroybuliro@gmail.com>
2022-09-08 09:14:40 -0700

    adding 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(void) {
-  char  str1[54];
-  strcpy(str1, "\"Programming is like building a multilingual puzzle");
-  puts(str1);
-  return (0);
+int main(void)
+{
+char  str1[54];
+strcpy(str1, "\"Programming is like building a multilingual puzzle");
+puts(str1);
+return (0);
 }
