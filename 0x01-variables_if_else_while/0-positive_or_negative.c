c411deaf72cc85351c89fec499a5f4c739d4d974
leroysb <leroybuliro@gmail.com>
2022-09-09 08:51:43 -0700

    adding 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,12 @@
+#include <stdio.h)
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
 /**
 * main - Entry point
 *
 * Return: Always 0 (Success)
 */
-main(void)
+int main(void)
 {
 int n;
 srand(time(0));
