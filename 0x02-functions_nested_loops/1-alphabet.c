c8449c4f691a78e5ef56743f2463dd34a6d058dd
leroysb <leroybuliro@gmail.com>
2022-09-14 14:00:28 -0700

    1-alphabet.c 1-main.c added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_alphabet - prints alphabet followed by a new line
+*
+* Return: void
+*/
+void print_alphabet(void)
+{
+char chr;
+chr = 'a';
+while (chr <= 'z')
+{
+_putchar(chr);
+chr++;
+}
+_putchar('\n');
+}
