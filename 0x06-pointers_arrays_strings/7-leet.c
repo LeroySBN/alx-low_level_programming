72da24ae4d3bd9cac8f37b6e54a5761fb8981073
leroysb <leroybuliro@gmail.com>
2022-09-22 15:46:17 -0700

    updating task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,23 +2,25 @@
 
 /**
  * leet - encodes a string into 1337.
- * @s: string to be encoded
  *
+ * @s: string to be encoded
  * Return: the resulting string;
  */
 char *leet(char *s)
 {
-	int i, j;
+	int i = 0;
+	int j;
 	char *a = "aAeEoOtTlL";
 	char *b = "4433007711";
 
-	for (i = 0; s[i] != '\0'; i++)
+	while  (s[i] != '\0')
 	{
 		for (j = 0; j < 10; j++)
 		{
 			if (s[i] == a[j])
 				s[i] = b[j];
 		}
+		i++;
 	}
 
 	return (s);
