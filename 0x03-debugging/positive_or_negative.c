7fefa6168891548b5172f573f7ba2508f696e2cf
leroysb <leroybuliro@gmail.com>
2022-09-28 05:31:57 -0700

    indent modification

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -8,21 +8,22 @@
 */
 int main(void)
 {
-int i;
-srand(time(0));
-i = rand() - RAND_MAX / 2;
-/* your code goes there */
-if (i > 0)
-{
-printf("%d is positive\n", i);
-}
-else if (i == 0)
-{
-printf("%d is zero\n", i);
-}
-else if (i < 0)
-{
-printf("%d is negative\n", i);
-}
-return (0);
+	int i;
+
+	srand(time(0));
+	i = rand() - RAND_MAX / 2;
+
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i == 0)
+	{
+		printf("%d is zero\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	return (0);
 }
